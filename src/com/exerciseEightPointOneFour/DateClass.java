78f62c11281a0d1684d15add6678ad9eb5c33246
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-05-06 23:27:11 +0100

    Date Class

---
+++ src/com/exerciseEightPointOneFour/DateClass.java
@@ -0,0 +1,83 @@
+package com.exerciseEightPointOneFour;
+//8.14 (Date Class) Create class Date with the following capabilities:
+//        a) Output the date in multiple formats, such as
+//        MM/DD/YYYY
+//        June 14, 1992
+//        DDD YYYY
+//        b) Use overloaded constructors to create Date objects initialized with dates of the formats
+//        in part (a). In the first case the constructor should receive three integer values. In the
+//        second case it should receive a String and two integer values. In the third case it should
+//        receive two integer values, the first of which represents the day number in the year.
+//        [Hint: To convert the String representation of the month to a numeric value, compare
+//        Strings using the equals method. For example, if s1 and s2 are Strings, the method
+//        call s1.equals(s2) returns true if the Strings are identical and otherwise returns
+//        false.]
+
+
+public class DateClass {
+    private int day;
+    private int month;
+    private int year;
+    private String monthInWords;
+    private int totalDays;
+
+    public DateClass() {
+    }
+
+    public DateClass(int month, int day, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public DateClass(String monthInWords, int day, int year) {
+        this.day = day;
+        this.year = year;
+        this.monthInWords = monthInWords;
+    }
+
+    public DateClass(int totalDays,int year) {
+        this.year = year;
+        this.totalDays = totalDays;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMonthInWords() {
+        return monthInWords;
+    }
+
+    public void setMonthInWords(String monthInWords) {
+        this.monthInWords = monthInWords;
+    }
+
+    public int getTotalDays() {
+        return totalDays;
+    }
+
+    public void setTotalDays(int totalDays) {
+        this.totalDays = totalDays;
+    }
+}
---
+++ test/com/exerciseEightPointOneFour/DateClassTest.java
@@ -0,0 +1,53 @@
+package com.exerciseEightPointOneFour;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateClassTest {
+    DateClass dateClass;
+
+    @BeforeEach
+    void setUp() {
+        dateClass = new DateClass();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void dateClassCanSetAndGotten(){
+        dateClass.setDay(3);
+        assertEquals(3, dateClass.getDay());
+        dateClass.setMonth(4);
+        assertEquals(4, dateClass.getMonth());
+        dateClass.setYear(2021);
+        assertEquals(2021, dateClass.getYear());
+        dateClass.setMonthInWords("may");
+        assertEquals("may", dateClass.getMonthInWords());
+        dateClass.setTotalDays(300);
+        assertEquals(300, dateClass.getTotalDays());
+    }
+
+    @Test
+    void dateClassForMonthDayYearCanBeGotten(){
+        dateClass = new DateClass(12,22,2021);
+        assertEquals(12,dateClass.getMonth());
+        assertEquals(22,dateClass.getDay());
+        assertEquals(2021, dateClass.getYear());
+        dateClass = new DateClass("June",3, 2019);
+        assertEquals("June", dateClass.getMonthInWords());
+        assertEquals(3, dateClass.getDay());
+        assertEquals(2019, dateClass.getYear());
+        dateClass = new DateClass(365,2020);
+        assertEquals(365,dateClass.getTotalDays());
+        assertEquals(2020, dateClass.getYear());
+
+
+    }
+
+
+}
